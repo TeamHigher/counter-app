4a7fd0f23531c144cb553dae787e9b9f8478b150
Uwabuofu Chukuka <chuksdavis2020@gmail.com>
2023-10-29 01:20:00 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CounterPage from "./Routes/counterPage";
 import PageNotFound from "./Routes/pageNotFound";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/error">Error Boundary Test</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route exact path="/" element={<CounterPage />} />
         <Route path="*" element={<PageNotFound />} />
